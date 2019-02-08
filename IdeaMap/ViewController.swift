b735ba8913d868dbd10b04df09014d8c00c84f3f
Aoi Saito <aoisaito@aoinomacbook-ea.local>
2019-02-08 20:02:57 +0900

    Initial Commit

---
+++ IdeaMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IdeaMap
+//
+//  Created by Aoi Saito on 2019/02/08.
+//  Copyright © 2019年 Aoi Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
