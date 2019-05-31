261837c7fc0053d784b2226032d877552520d021
AoiblueSaito <aoibluesaito@gmail.com>
2019-05-31 19:01:21 +0900

    first

---
+++ IdeaMap/IdeaMapViewController.swift
@@ -0,0 +1,40 @@
+//
+//  IdeaMapViewController.swift
+//  IdeaMap
+//
+//  Created by Aoi Saito on 2019/02/22.
+//  Copyright © 2019年 Aoi Saito. All rights reserved.
+//
+
+import UIKit
+
+class IdeaMapViewController: UIViewController {
+
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var contentTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resorces that can be recreated
+        
+    }
+    
+    @IBAction func saveMemo
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- IdeaMap/ViewController.swift
+++ IdeaMap/ViewController.swift
@@ -8,13 +8,91 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-  
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = table.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //セルの縦横の長さ
+        table.dataSource = self
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 80, height: 80)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        table.collectionViewLayout = layout
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+   @IBOutlet var table: UICollectionView!
+    
+    //数指定
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return 9
     }
 
-
+    class IdeaMapViewController: UIViewController{
+        
+        @IBOutlet var titleTextField: UITextField!
+        @IBOutlet var contentTextView: UITextView!
+        
+        var saveDate: UserDefaults = UserDefaults.standard
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            titleTextField.text = saveDate.set(titleTextField.text, forkey: "title") as? String
+            contentTextView.text = saveDate.set(contentTextView.text, forkey: "content")as? String
+            
+            
+            //Do any additional setup after loading the view.
+            
+            @IBOutlet func saveMemo() {
+                //UserDefaultsに書き込み
+                saveDate.set(titleTextField.text, forkey: "tite")
+                saveDate.set(contentTextView.text, forkey: "content")
+                
+            }
+        }
+        @IBOutlet func saveMemo() {
+            
+            //UserDefaultに書き込み
+            saveDate.set(titleTextField.text, forkey: "title")
+            saveDate.set(contentTextView.text, forkey: "content")
+            
+            //alertをだす
+            let alert: UIAlertController = UIAlertController(title: "保存", message: "内容の保存が完了しました", preferredStyle: .alert)
+            
+            //OKボタン
+            alert.addAction(
+                UIAlertAction(title: "OK",
+                              style: .default,
+                    handler: { action in
+                        //ボタンが押された時の動作
+                        print("OKボタンが押されました")
+                }
+                )
+            )
+            present(alert, animated: true, completion: nil)
+            
+            //ボタンが押された時の動作
+            self.navigationController?.popViewController(animated: true)
+            
+        }
+        
+    }
+    
+    
 }
 
